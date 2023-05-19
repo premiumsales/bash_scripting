206f7ab025c97daf594ed218b32d372def87be65
Christopher Ozurumba <67044030+Mamiololo01@users.noreply.github.com>
2023-05-20 00:15:41 +0100

    Update For_loop.sh

--- For_loop.sh
+++ For_loop.sh
@@ -22,3 +22,18 @@ do
         # we are here means no backup file exists, just use cp command to copy file
     /bin/cp $f $f.bak
 done
+
+
+#!/usr/bin/env bash
+# Purpose: Update all my Linode servers powered by Debian/Ubuntu Linux
+# Author: Vivek Gite under GPL v2.x+
+# ----------------------------------------
+log="/tmp/apt-get.log"
+>"${log}"
+for s in ln.cbz0{1..5}
+do
+   echo "Updating and patching $s, please wait..." | tee -a "${log}"
+   ssh root@${s} -- apt-get -q -y update >/dev/null
+   ssh root@${s} -- DEBIAN_FRONTEND=noninteractive apt-get -y -q upgrade >>"${log}"
+done
+echo "Check $log file for details

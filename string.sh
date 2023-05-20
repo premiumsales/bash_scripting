c0aedcf91773d05af423be9c8e66a80e8a8adcb4
Christopher Ozurumba <67044030+Mamiololo01@users.noreply.github.com>
2023-05-20 13:08:57 +0100

    Update string.sh

--- string.sh
+++ string.sh
@@ -15,3 +15,24 @@ else
 
 fi
 
+#! /bin/bash
+
+    echo "enter 1st string"
+read st1
+    echo "enter 2nd string"
+read st2
+
+if [ "$st1" \< "$st2" ]
+
+then 
+    echo "$st1 is smaller than $st2"
+
+elif [ "$st1" \> "$st2" ]
+then 
+    echo "$st2 is smaller than $st1"
+else
+    echo "both string are equal"
+fi
+
+
+

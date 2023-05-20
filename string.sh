0638ea21e0fa2d0e5e2be6fc81c83fb345479250
Christopher Ozurumba <67044030+Mamiololo01@users.noreply.github.com>
2023-05-20 12:42:55 +0100

    Create string.sh

---
+++ string.sh
@@ -0,0 +1,17 @@
+#! /bin/bash
+
+    echo "enter 1st string"
+read st1
+    echo "enter 2nd string"
+read st2
+
+if [ "$st1" == "$st2" ]
+
+then 
+    echo "both strings match"
+
+else 
+    echo "strings dont match"
+
+fi
+

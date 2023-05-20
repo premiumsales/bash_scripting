92216c67c46299fc8c24c70c57ea0fedea1e38df
Christopher Ozurumba <67044030+Mamiololo01@users.noreply.github.com>
2023-05-20 01:22:13 +0100

    Update user_input.sh

--- user_input.sh
+++ user_input.sh
@@ -3,3 +3,10 @@
 echo Hello, who am I talking to?
 read varname
 echo It\'s nice to meet you $varname
+
+#!/bin/bash
+# Ask the user for login details
+read -p 'Username: ' uservar
+read -sp 'Password: ' passvar
+echo
+echo Thankyou $uservar we now have your login details

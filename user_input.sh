f96d44dbe10da52ce1aae5a300ad2c03e8653f58
Christopher Ozurumba <67044030+Mamiololo01@users.noreply.github.com>
2023-05-20 01:44:49 +0100

    Update user_input.sh

--- user_input.sh
+++ user_input.sh
@@ -10,3 +10,10 @@ read -p 'Username: ' uservar
 read -sp 'Password: ' passvar
 echo
 echo Thankyou $uservar we now have your login details
+
+#!/bin/bash
+# A basic summary of my sales report
+echo Here is a summary of the sales data:
+echo ====================================
+echo
+cat /dev/stdin | cut -d' ' -f 2,3 | sort

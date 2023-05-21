58d281dba9937de21df614150af99a90204be280
Christopher Ozurumba <67044030+Mamiololo01@users.noreply.github.com>
2023-05-21 16:43:50 +0100

    Update array_script.sh

--- array_script.sh
+++ array_script.sh
@@ -3,3 +3,11 @@
 car=("BMW" "Camry" "Honda" "Mercedes" "Daewoo")
 echo "${car[@]}"
 
+#! /bin/bash
+
+car=("BMW" "Camry" "Honda" "Mercedes" "Daewoo")
+unset car[2]
+echo "${car[@]}"
+echo "${car[0]}"
+echo "${#car[@]}"
+

ced1290c634c56bd5fde44b2606b7337b6339ef0
Christopher Ozurumba <67044030+Mamiololo01@users.noreply.github.com>
2023-05-23 18:16:22 +0100

    Update awk.sh

--- awk.sh
+++ awk.sh
@@ -15,3 +15,10 @@ then
    echo "$fileName doesnt exist"
 
 fi
+
+# To print specific words eg windows
+awk '/windows/{print} $fileName
+
+#To print specific field
+awk '/windows/{print$2} $fileName
+
